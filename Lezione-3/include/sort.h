ca13f33aee98d501d2a69a8b6f19ad20f5fe4793
20023866 <20023866@lab30.edu-al.unipmn.it>
2018-11-21 15:40:54 +0100

    Completed sort.h

--- Lezione-3/include/sort.h
+++ Lezione-3/include/sort.h
@@ -1,1 +1,11 @@
- 
+#include <stdio.h> 
+#include <stdlib.h>
+
+void bubbleSort (int* A, int n);
+
+void quicksort(int* A, int begin, int end);
+int partition(int* A, int begin, int end);
+
+void fixHeap(int* A, int i, int HeapSize);
+void heapify(int* A, int n);
+void heapSort(int* A, int n);
--- Lezione-3/src/sort.c
+++ Lezione-3/src/sort.c
@@ -1,1 +1,3 @@
+#include "sort.h"
+
  
