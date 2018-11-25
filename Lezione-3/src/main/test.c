71197b9fe8025fd573419a9a92f16fddba60d2f6
Davide <davidesavarro98@gmail.com>
2018-11-25 19:06:40 +0100

    isSorted and malloc checks
    Added isSorted function in test.c and added come checks in mallocs of utils.c. Also added srand(time(NULL)) to creaArrayRandom

--- Lezione-3/src/main/test.c
+++ Lezione-3/src/main/test.c
@@ -1,11 +1,35 @@
 #include "sort.h" 
 #include "utils.h"
+#include <stdbool.h>
+
+// Controlla se l'array è ordinato
+bool isSorted(int* A, int n){
+	for(int i=0; i<n-1; i+=1){
+		if(A[i] > A[i+1]) return false;
+	}
+	return true;
+}
 
 int main(int argc, char* argv[]){
 	int* A = creaArrayRandom(10);
 	int* B = creaArrayRandom(10);
 	int* C = creaArrayRandom(10);
+
+	fprintArray(stdout, A, 10);
+	fprintArray(stdout, B, 10);
+	fprintArray(stdout, C, 10);
 	
+	if(!isSorted(A,10)) printf("false\n");
+	else printf("true\n");
+
+	if(!isSorted(B,10)) printf("false\n");
+	else printf("true\n");
+
+	if(!isSorted(C,10)) printf("false\n");
+	else printf("true\n");
+	
+	printf("...SORTING...\n");
+	// SORT
 	quicksort(A, 0, 9);
 	bubbleSort(B, 10);
 	heapSort(C, 10);
@@ -13,5 +37,15 @@ int main(int argc, char* argv[]){
 	fprintArray(stdout, A, 10);
 	fprintArray(stdout, B, 10);
 	fprintArray(stdout, C, 10);
+	
+	if(!isSorted(A,10)) printf("false\n");
+	else printf("true\n");
+
+	if(!isSorted(B,10)) printf("false\n");
+	else printf("true\n");
+
+	if(!isSorted(C,10)) printf("false\n");
+	else printf("true\n");
+
 }
 
--- Lezione-3/src/utils.c
+++ Lezione-3/src/utils.c
@@ -2,32 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include "utils.h"
-
+#include <time.h>
 
 int* creaArrayRandom(int dim) {
 	// ALLOC
 	int* arr = malloc(dim * sizeof(int));
-	for(int i=0; i < dim; i+=1)
+	if(arr==NULL){
+		printf("Errore allocazione array creazione\n");
+		exit(1);
+	}
+	
+	srand(time(NULL));
+	for(int i=0; i < dim; i+=1){
 		arr[i] = rand();
+	}
 	return arr;
 }
 
 int* copyArray(int* A, int dim) {
 	int* arr = malloc(dim * sizeof(int));
+	if(arr==NULL){
+		printf("Errore allocazione array copia\n");
+		exit(1);
+	}
 	memcpy(arr, A, dim * sizeof(int));
 	return arr;
 }
 
 void fprintArray(FILE* f, int* A, int dim) {
 	printf("[");
-	if(dim > 0)
+	if(dim > 0){
 		fprintf(f,"%d", A[0]);
-	
+	}
 	// attenzione, si inizia da 1 perche' il primo elemento
-	// (se esistente) e' gia' stato stampato
-	for(int i=1; i < dim; i+=1)
+	// (se esiste) e' gia' stato stampato
+	for(int i=1; i < dim; i+=1){
 		fprintf(f,", %d", A[i]);
-		
+	}
 	printf("]\n");
 }
 
