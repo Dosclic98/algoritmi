4308aca5c7ad2f9bd408aadc3d8994aa4b47418f
20023866 <20023866@lab20.edu-al.unipmn.it>
2018-12-05 16:17:02 +0100

    Tempo da aggiungere

--- Lezione-5/src/main/eserc.c
+++ Lezione-5/src/main/eserc.c
@@ -4,12 +4,17 @@
 
 
 int main(int argc, char** args) {
-	int dimArray;
-	int* array;
-	Nodo* radice = NULL;
+	int dims[8] = {10,25,50,100,200,500,1000,2000};
+	
+	for(int i=0;i<1;i++){
+		int dimArray = dims[i];
+		int* array = creaArrayRandom(dimArray);
+		Nodo* radice = NULL;
+		radice = creaAlbero(array, dimArray);
+		
+		quicksort(array,0,dimArray-1);
+		fprintArray(stdout,array,dimArray);
+		printAlbero(radice);
 
-	// CONTROLLA E PROCESSA GLI ARGOMENTI
-
-	array = creaArrayRandom();
-	radice = creaAlbero();
+	}
 }
