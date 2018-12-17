ed9ca4e96d2c748edcd19f2c5417d5612743349e
20023866 <20023866@lab29.edu-al.unipmn.it>
2018-12-17 11:00:56 +0100

    Search and insert finished

--- Lezione-6/src/hash.c
+++ Lezione-6/src/hash.c
@@ -9,6 +9,16 @@ int hashInsert(bucket* T[], bucket* e, int m){
 			return j;
 		}
 		else i++;
-	}while(i<m)
+	}while(i<m);
 	return -1;
-} 
+}
+
+int hashSearch(bucket* T[], bucket* e, int m){
+	int i=0;
+	do{
+		int j = h(e->key,i,m);
+		if(T[j] == e->key) return j;
+		else i++;
+	}while( (T[j]!=NULL) && i<m);
+	return -1;
+}
